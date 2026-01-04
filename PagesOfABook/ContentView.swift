2f90a41ad1adaae8114c0fbde42026f3bc0d29f4
feed0 <dogmas-gutters.07@icloud.com>
2026-01-04 10:04:26 -0300

    Initial Commit

---
+++ PagesOfABook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PagesOfABook
+//
+//  Created by Felipe Eduardo Campelo Ferreira Osorio on 04/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PagesOfABook/PagesOfABookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PagesOfABookApp.swift
+//  PagesOfABook
+//
+//  Created by Felipe Eduardo Campelo Ferreira Osorio on 04/01/26.
+//
+
+import SwiftUI
+
+@main
+struct PagesOfABookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
